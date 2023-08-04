9a3571a74b48629c65c1bab195f69312b5eb7739
bhoomildayani182 <bhoomildayani182@gmail.com>
2023-08-05 00:11:21 +0530

    Update File

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //const mongoURI = "mongodb://localhost:27017/BookMyTable?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
-const mongoURI = "mongodb+srv://bhoomildayani05:Bhoomil1234@cluster0.j0zwb7i.mongodb.net/"
+
 
 const connetToMongo = () => {
     mongoose.connect(mongoURI, () => {
